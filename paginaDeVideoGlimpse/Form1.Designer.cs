824d56823461c7ac92009ff0039ab68aa94f997c
adrianareyes134 <reyes.h.adriana.loyola@gmail.com>
2025-07-15 13:55:09 -0600

    Agregar archivos de proyecto.

---
+++ paginaDeVideoGlimpse/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace paginaDeVideoGlimpse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnsalirdevideos = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.GlimpseNow = new System.Windows.Forms.Button();
+            this.btnVideo = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btnglimpse1 = new System.Windows.Forms.Button();
+            this.btncambiar1 = new System.Windows.Forms.Button();
+            this.btnlike = new System.Windows.Forms.Button();
+            this.btncambiar2 = new System.Windows.Forms.Button();
+            this.btnshare = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnicono = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnstory1 = new System.Windows.Forms.Button();
+            this.btnstory2 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.btnVideo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnsalirdevideos
+            // 
+            this.btnsalirdevideos.FlatAppearance.BorderSize = 0;
+            this.btnsalirdevideos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnsalirdevideos.Image = ((System.Drawing.Image)(resources.GetObject("btnsalirdevideos.Image")));
+            this.btnsalirdevideos.Location = new System.Drawing.Point(168, 102);
+            this.btnsalirdevideos.Margin = new System.Windows.Forms.Padding(7, 9, 7, 9);
+            this.btnsalirdevideos.Name = "btnsalirdevideos";
+            this.btnsalirdevideos.Size = new System.Drawing.Size(153, 36);
+            this.btnsalirdevideos.TabIndex = 0;
+            this.btnsalirdevideos.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Myanmar Text", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(67)))), ((int)(((byte)(109)))));
+            this.label2.Location = new System.Drawing.Point(750, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 48);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "GlimpseNow!";
+            // 
+            // GlimpseNow
+            // 
+            this.GlimpseNow.BackColor = System.Drawing.Color.Transparent;
+            this.GlimpseNow.FlatAppearance.BorderSize = 0;
+            this.GlimpseNow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GlimpseNow.Image = ((System.Drawing.Image)(resources.GetObject("GlimpseNow.Image")));
+            this.GlimpseNow.Location = new System.Drawing.Point(771, 343);
+            this.GlimpseNow.Name = "GlimpseNow";
+            this.GlimpseNow.Size = new System.Drawing.Size(244, 36);
+            this.GlimpseNow.TabIndex = 3;
+            this.GlimpseNow.UseVisualStyleBackColor = false;
+            // 
+            // btnVideo
+            // 
+            this.btnVideo.Enabled = true;
+            this.btnVideo.Location = new System.Drawing.Point(214, 150);
+            this.btnVideo.Name = "btnVideo";
+            this.btnVideo.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("btnVideo.OcxState")));
+            this.btnVideo.Size = new System.Drawing.Size(251, 490);
+            this.btnVideo.TabIndex = 5;
+            this.btnVideo.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // btnglimpse1
+            // 
+            this.btnglimpse1.FlatAppearance.BorderSize = 0;
+            this.btnglimpse1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnglimpse1.Image = ((System.Drawing.Image)(resources.GetObject("btnglimpse1.Image")));
+            this.btnglimpse1.Location = new System.Drawing.Point(697, 125);
+            this.btnglimpse1.Name = "btnglimpse1";
+            this.btnglimpse1.Size = new System.Drawing.Size(55, 55);
+            this.btnglimpse1.TabIndex = 7;
+            this.btnglimpse1.UseVisualStyleBackColor = true;
+            // 
+            // btncambiar1
+            // 
+            this.btncambiar1.FlatAppearance.BorderSize = 0;
+            this.btncambiar1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncambiar1.Image = ((System.Drawing.Image)(resources.GetObject("btncambiar1.Image")));
+            this.btncambiar1.Location = new System.Drawing.Point(486, 276);
+            this.btncambiar1.Name = "btncambiar1";
+            this.btncambiar1.Size = new System.Drawing.Size(23, 23);
+            this.btncambiar1.TabIndex = 8;
+            this.btncambiar1.UseVisualStyleBackColor = true;
+            // 
+            // btnlike
+            // 
+            this.btnlike.FlatAppearance.BorderSize = 0;
+            this.btnlike.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlike.Image = ((System.Drawing.Image)(resources.GetObject("btnlike.Image")));
+            this.btnlike.Location = new System.Drawing.Point(480, 331);
+            this.btnlike.Name = "btnlike";
+            this.btnlike.Size = new System.Drawing.Size(36, 32);
+            this.btnlike.TabIndex = 10;
+            this.btnlike.UseVisualStyleBackColor = true;
+            // 
+            // btncambiar2
+            // 
+            this.btncambiar2.FlatAppearance.BorderSize = 0;
+            this.btncambiar2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncambiar2.Image = ((System.Drawing.Image)(resources.GetObject("btncambiar2.Image")));
+            this.btncambiar2.Location = new System.Drawing.Point(486, 449);
+            this.btncambiar2.Name = "btncambiar2";
+            this.btncambiar2.Size = new System.Drawing.Size(23, 24);
+            this.btncambiar2.TabIndex = 12;
+            this.btncambiar2.UseVisualStyleBackColor = true;
+            // 
+            // btnshare
+            // 
+            this.btnshare.FlatAppearance.BorderSize = 0;
+            this.btnshare.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnshare.Image = ((System.Drawing.Image)(resources.GetObject("btnshare.Image")));
+            this.btnshare.Location = new System.Drawing.Point(480, 387);
+            this.btnshare.Name = "btnshare";
+            this.btnshare.Size = new System.Drawing.Size(36, 31);
+            this.btnshare.TabIndex = 11;
+            this.btnshare.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(758, 191);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(343, 196);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnicono
+            // 
+            this.btnicono.BackColor = System.Drawing.Color.Transparent;
+            this.btnicono.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnicono.BackgroundImage")));
+            this.btnicono.FlatAppearance.BorderSize = 0;
+            this.btnicono.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnicono.Location = new System.Drawing.Point(3, 12);
+            this.btnicono.Name = "btnicono";
+            this.btnicono.Size = new System.Drawing.Size(73, 55);
+            this.btnicono.TabIndex = 14;
+            this.btnicono.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(28, 551);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 165);
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(945, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(119, 45);
+            this.pictureBox3.TabIndex = 16;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnstory1
+            // 
+            this.btnstory1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnstory1.BackgroundImage")));
+            this.btnstory1.FlatAppearance.BorderSize = 0;
+            this.btnstory1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnstory1.Location = new System.Drawing.Point(945, 487);
+            this.btnstory1.Name = "btnstory1";
+            this.btnstory1.Size = new System.Drawing.Size(102, 153);
+            this.btnstory1.TabIndex = 1;
+            this.btnstory1.UseVisualStyleBackColor = true;
+            // 
+            // btnstory2
+            // 
+            this.btnstory2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnstory2.BackgroundImage")));
+            this.btnstory2.FlatAppearance.BorderSize = 0;
+            this.btnstory2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnstory2.Location = new System.Drawing.Point(820, 487);
+            this.btnstory2.Name = "btnstory2";
+            this.btnstory2.Size = new System.Drawing.Size(102, 153);
+            this.btnstory2.TabIndex = 2;
+            this.btnstory2.UseVisualStyleBackColor = true;
+            this.btnstory2.Click += new System.EventHandler(this.btnstory2_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(765, 450);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(335, 232);
+            this.pictureBox4.TabIndex = 17;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.btnstory1);
+            this.Controls.Add(this.btnstory2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnicono);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btncambiar2);
+            this.Controls.Add(this.btnshare);
+            this.Controls.Add(this.btnlike);
+            this.Controls.Add(this.btncambiar1);
+            this.Controls.Add(this.btnglimpse1);
+            this.Controls.Add(this.btnVideo);
+            this.Controls.Add(this.GlimpseNow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnsalirdevideos);
+            this.Controls.Add(this.pictureBox4);
+            this.Font = new System.Drawing.Font("Myanmar Text", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 9, 7, 9);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.btnVideo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsalirdevideos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button GlimpseNow;
+        private AxWMPLib.AxWindowsMediaPlayer btnVideo;
+        private System.Windows.Forms.Button btnglimpse1;
+        private System.Windows.Forms.Button btncambiar1;
+        private System.Windows.Forms.Button btnlike;
+        private System.Windows.Forms.Button btncambiar2;
+        private System.Windows.Forms.Button btnshare;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnicono;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnstory1;
+        private System.Windows.Forms.Button btnstory2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ paginaDeVideoGlimpse/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paginaDeVideoGlimpse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnstory2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
